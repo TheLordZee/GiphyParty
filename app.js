d24541ff1ace11f26d5c1b39bcd723e6bdfad745
LordZee <l0rclz33@gmail.com>
2021-03-11 13:48:01 -0500

    finish exercise

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 console.log("Let's get this party started!");
-const key = 'fZeywUi3SkjitarEkQHWq2B2GO5TZyGx';
 
+//Handles form submission
 $('#search').on('submit', (e) => {
     e.preventDefault();
-    makeGif($('#searchTerm').val());
+    const search = $('#searchTerm').val();
+    getGif(search);
+    $('#searchTerm').val('');
 })
 
-async function makeGif(q) {
-    const res = await axios.get('api.giphy.com/v1/gifs/search', { params: { api_key: key, q } });
-    console.log(res);
+
+//Gets the url of the gif
+async function getGif(q) {
+    try {
+        const res = await axios.get('http://api.giphy.com/v1/gifs/search', {
+            params: {
+                api_key: 'fZeywUi3SkjitarEkQHWq2B2GO5TZyGx',
+                q
+            }
+        });
+        const gifs = res.data.data;
+        const ranId = Math.floor(Math.random() * (gifs.length - 1));
+        const url = gifs[ranId].images.original.url;
+        setGif(url)
+        console.log(res.data.data);
+    } catch (e) {
+        alert('Request Failed!')
+    }
 }
+
+//Creates and appends the gif to the page
+function setGif(url) {
+    let $newCol = $("<div>", { class: "col-12 col-md-4 col-lg-3" });
+    let $newGif = $("<img>", {
+        src: url
+    })
+    $newCol.append($newGif);
+    $('#gif-row').append($newCol);
+}
+
+//Removes Gifs
+$("#delete").on("click", function () {
+    $('#gif-row').empty();
+});
